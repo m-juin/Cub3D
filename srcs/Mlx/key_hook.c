55f7b03f7db7419fa10d2dc15e2f9fdd6b41d31f
Mickael Juin <mjuin@2D1.42angouleme.fr>
2023-05-08 15:08:06 +0200

    Adding repeat key

--- srcs/Mlx/key_hook.c
+++ srcs/Mlx/key_hook.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   key_hook.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lobozier <lobozier@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mjuin <mjuin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 12:15:25 by mjuin             #+#    #+#             */
-/*   Updated: 2023/05/08 12:29:32 by lobozier         ###   ########.fr       */
+/*   Updated: 2023/05/08 15:07:32 by mjuin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static void	movement_hook(mlx_key_data_t keydata, t_player	*player, t_data *data
 	t_dvector target;
 
 	if (keydata.key == MLX_KEY_W
-		&& keydata.action == MLX_RELEASE)
+		&& (keydata.action == MLX_PRESS || keydata.action == MLX_REPEAT))
 	{
 		target = collide(player, data, keydata);
 		if (target.x == player->map_pos.x && target.y == player->map_pos.y)
@@ -26,7 +26,7 @@ static void	movement_hook(mlx_key_data_t keydata, t_player	*player, t_data *data
 		player->canmove = false;
 	}	
 	else if (keydata.key == MLX_KEY_S
-		&& keydata.action == MLX_RELEASE)
+		&& (keydata.action == MLX_PRESS || keydata.action == MLX_REPEAT))
 	{
 		target = collide(player, data, keydata);
 		if (target.x == player->map_pos.x && target.y == player->map_pos.y)
@@ -35,7 +35,7 @@ static void	movement_hook(mlx_key_data_t keydata, t_player	*player, t_data *data
 		player->canmove = false;
 	}
 	else if (keydata.key == MLX_KEY_D
-		&& (keydata.action == MLX_REPEAT || keydata.action == MLX_PRESS))
+		&& (keydata.action == MLX_PRESS || keydata.action == MLX_REPEAT))
 	{
 		target = collide(player, data, keydata);
 		if (target.x == player->map_pos.x && target.y == player->map_pos.y)
@@ -44,7 +44,7 @@ static void	movement_hook(mlx_key_data_t keydata, t_player	*player, t_data *data
 		player->canmove = false;
 	}	
 	else if (keydata.key == MLX_KEY_A
-		&& (keydata.action == MLX_REPEAT || keydata.action == MLX_PRESS))
+		&& (keydata.action == MLX_PRESS || keydata.action == MLX_REPEAT))
 	{
 		target = collide(player, data, keydata);
 		if (target.x == player->map_pos.x && target.y == player->map_pos.y)
@@ -58,7 +58,7 @@ static void	rotation_hook(mlx_key_data_t keydata, t_player *player, t_data *data
 {
 
 	if (keydata.key == MLX_KEY_LEFT
-		&& keydata.action == MLX_RELEASE)
+		&& (keydata.action == MLX_PRESS || keydata.action == MLX_REPEAT))
 	{
 		if (player->facing_dir > 0)
 			player->target_dir -= 1;
@@ -67,7 +67,7 @@ static void	rotation_hook(mlx_key_data_t keydata, t_player *player, t_data *data
 		player->canmove = false;
 	}
 	else if (keydata.key == MLX_KEY_RIGHT
-		&& keydata.action == MLX_RELEASE)
+		&& (keydata.action == MLX_PRESS || keydata.action == MLX_REPEAT))
 	{
 		if (player->facing_dir < 3)
 			player->target_dir += 1;
